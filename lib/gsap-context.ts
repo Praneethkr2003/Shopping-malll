ff7a850a4c5ae4535583112e4f9846815ce7c04a
Radiant Los <163734634+Praneethkr2003@users.noreply.github.com>
2026-05-30 13:15:31 +0530

    Delete lib directory

--- lib/gsap-context.ts
+++
@@ -1,8 +0,0 @@
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { TextPlugin } from 'gsap/TextPlugin';
-
-// Register plugins
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
-
-export { gsap, ScrollTrigger };
